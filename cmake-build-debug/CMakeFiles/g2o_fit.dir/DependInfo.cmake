d3528fd4b71264df1e7a300824745f7c648982ad
elfsa-code <elfsaikou@gmail.com>
2023-06-20 14:53:01 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/g2o_fit.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elfsa/g2o_fit/g2o_fit.cpp" "/home/elfsa/g2o_fit/cmake-build-debug/CMakeFiles/g2o_fit.dir/g2o_fit.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
